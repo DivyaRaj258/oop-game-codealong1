cb52b9ebd87bdc83f4ced31b6c3df293c7cb4241
DivyaRaj258 <divya.r258@gmail.com>
2023-08-10 18:03:44 +0200

    some changes

--- js/main.js
+++ js/main.js
@@ -5,38 +5,30 @@ class Player {
         this.width = 20;
         this.height = 10;
         this.domElement = null;
-
         this.createDomElement();
     }
     createDomElement(){
         // create dom element
         this.domElement = document.createElement("div");
-
         // set id
         this.domElement.id = "player";
-        this.domElement.style.width = this.width + "vw"
+        this.domElement.style.width = this.width + "vw";
         this.domElement.style.height = this.height + "vh";
-        this.domElementdomElement.style.left = this.positionX + "vw";
+        this.domElement.style.left = this.positionX + "vw";
         this.domElement.style.bottom = this.positionY + "vh";
-
         //append to the dom
         const parentElm = document.getElementById("board");
-        parentElm.appendChild(newDiv);
+        parentElm.appendChild(this.domElement);
     }
     moveLeft(){
         this.positionX--;
-        this.domElement.style.left = this.positionX + "vm";
-
-        //console.log(`moving left... ${this.positionX} `);
+        this.domElement.style.left = this.positionX + "vw";
     }
     moveRight(){
         this.positionX++;
-        this.domElement.style.right = this.positionX +"vm";
-
-        //console.log(`moving right... ${this.positionX} `);
+        this.domElement.style.left = this.positionX + "vw";
     }
 }
-
 class Obstacle {
     constructor(){
         this.positionX = 50;
@@ -44,79 +36,48 @@ class Obstacle {
         this.width = 20;
         this.height = 10;
         this.domElement = null;
-
         this.createDomElement();
-
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement("div");
+        // set id
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+        //append to the dom
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+    moveDown(){
+        this.positionY -= 2;
+        this.domElement.style.bottom = this.positionY + "vh";
     }
 }
-CreateDomElement() {
-    // create dom element
-    this.domElement = document.createElement("div");
-
-    // set id
-    this.domElement.className = "obstacle";
-    this.domElement.style.width = this.width + "vw"
-    this.domElement.style.height = this.height + "vh";
-    this.domElementdomElement.style.left = this.positionX + "vw";
-    this.domElement.style.bottom = this.positionY + "vh";
-
-    //append to the dom
-    const parentElm = document.getElementById("board");
-    parentElm.appendChild(newDiv);
-}
-moveDown(){
-   
-    this.positionY -= 2;
-    this.domElement.style.bottom = this.positionY + "vh";
-}
-
 const player = new Player();
-
-const obstacles =[];
-
-setInterval (() => {
-    const newObstacle = newObstacle();
+const obstaclesArr = [];  //will store instances of the class Obstacle
+// create obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
     obstaclesArr.push(newObstacle);
-    console.log("we have created a new obstacle...",obstaclesArr.length)
 }, 3000);
+// move all obstacles
+setInterval(() => {
+    obstaclesArr.forEach( (obstacleInstance) => {
+        obstacleInstance.moveDown();
 
+        if (
+            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            player.positionX + player.width > obstacleInstance.positionX &&
+            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            player.positionY + player.height > obstacleInstance.positionY
+        ) {
+            // Collision detected!
+            console.log("game over my fren! ");
+            location.href = "./gameover.html";
+        }
 
-
-const obstacleOne = new Obstacle ();
-setInterval(()=>){
-    obstacleOne.moveDown();
-
-obstaclesArr.forEach((obstacleInstance)=> {
-obstaclesInstance.moveDown();
-});
-
-if (
-    player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
-    player.positionX + player.width > obstacleInstance.positionX &&
-    player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
-    player.positionY + player.height > obstacleInstance.positionY
-) {
-    // Collision detected!
-    console.log("game over my fren! ");
-    location.href = ""
-},100);
-
-});
-
-
-
-
-obstacleOne.moveDown();
-obstacleOne.moveDown();
-obstacleOne.moveDown();
-
-
-
-
-document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft") {
-        player.moveLeft();
-    } else if (event.key === "ArrowRight") {
-        player.moveRight();
-    }
-});
+    });
+}, 100);
